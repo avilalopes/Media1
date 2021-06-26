e83d5e8f3e58686b3d6df468417fd4f98e6abfec
avilalopes <82402280+avilalopes@users.noreply.github.com>
2021-06-25 21:24:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+            double a, b, mediaP;
+
+            Console.WriteLine("Informe a nota A: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a nota B: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            
+            mediaP = ((a*3.5) + (b* 7.5)) / (3.5 + 7.5);
+
+            Console.WriteLine("MEDIA = {0}", mediaP.ToString("0.00000"));
+
+    }
+
+}
